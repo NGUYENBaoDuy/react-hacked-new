19b5ff138a5c370614823cf65e0656958d808a13
duynb <duynb@vng.com.vn>
2019-05-02 07:30:31 +0700

    add first midleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import rootReducer from '../reducers'
 
+const logger = createLogger();
 const store = createStore(
-    rootReducer
+    rootReducer,
+    undefined,
+    applyMiddleware(logger)
 );
 export default store;
